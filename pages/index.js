44e4b5ead1df8af2c5eb6323a2e97f0bb04d351f
xandemotta <85975693+xandemotta@users.noreply.github.com>
2024-05-27 05:29:25 +0000

    adicionado .prettierignore

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 function Home() {
   return <h1>Home</h1>;
 }
-function teste() {
+function teste2() {
   return <h1>Teste</h1>;
 }
-function teste() {
+function teste3() {
   return <h1>Teste</h1>;
 }
 export default Home;
